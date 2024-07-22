e7c11a4817859368c76fe479f51eacb4f33be6a1
DabP03 <piotrek.dabrowa@gmail.com>
2024-07-22 14:22:29 +0200

    scripts and README

--- scripts/send_one_to_bbb.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $# -eq 0 ]; then
-echo "Usage: bash send_to_bbb.sh [file]"
-else
-scp ./$1 debian@192.168.6.2:~
-fi
--- scripts/send_to_bbb.sh
+++ scripts/send_to_bbb.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./scripts/send_one_to_bbb.sh ./target/arm-unknown-linux-gnueabihf/debug/rusty_beagle
+scp ./target/arm-unknown-linux-gnueabihf/debug/rusty_beagle debian@192.168.6.2:~
