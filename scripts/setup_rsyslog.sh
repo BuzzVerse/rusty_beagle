9467da30156cc647eabd2460e58d7bb204cb09a4
DabP03 <piotrek.dabrowa@gmail.com>
2024-07-23 14:30:11 +0200

    logging works + updated rsyslog script

--- scripts/setup_rsyslog.sh
+++ scripts/setup_rsyslog.sh
@@ -3,6 +3,21 @@
 rusty_log="/etc/rsyslog.d/rusty_beagle.conf"
 
 sudo touch $rusty_log
-echo "if \$programname == 'rusty_beagle' then /var/log/rusty_beagle.log" | sudo tee $rusty_log
-echo "& stop" | sudo tee -a $rusty_log
+echo "template(name="rusty_beagle_list" type="list") {
+    property(name="timereported" dateFormat="rfc3339")
+    constant(value=" ")
+    property(name="hostname")
+    constant(value=" ")
+    property(name="app-name")
+    constant(value=" [")
+    property(name="syslogseverity-text")
+    constant(value="]")
+    constant(value=":")
+    property(name="msg" spifno1stsp="on" )
+    property(name="msg" droplastlf="on" )   
+    constant(value="\n")
+}
+
+if \$programname == 'rusty_beagle' then /var/log/rusty_beagle.log;rusty_beagle_list
+& stop" | sudo tee $rusty_log
 sudo systemctl restart rsyslog
