67f453fe42e7ff54a13675f80811ab07480d6c81
DabP03 <piotrek.dabrowa@gmail.com>
2024-07-23 14:33:00 +0200

    logging works + updated rsyslog script

--- scripts/setup_rsyslog.sh
+++ scripts/setup_rsyslog.sh
@@ -3,19 +3,19 @@
 rusty_log="/etc/rsyslog.d/rusty_beagle.conf"
 
 sudo touch $rusty_log
-echo "template(name="rusty_beagle_list" type="list") {
-    property(name="timereported" dateFormat="rfc3339")
+echo "template(name=\"rusty_beagle_list\" type=\"list\") {
+    property(name=\"timereported\" dateFormat=\"rfc3339\")
     constant(value=" ")
-    property(name="hostname")
-    constant(value=" ")
-    property(name="app-name")
-    constant(value=" [")
-    property(name="syslogseverity-text")
-    constant(value="]")
-    constant(value=":")
-    property(name="msg" spifno1stsp="on" )
-    property(name="msg" droplastlf="on" )   
-    constant(value="\n")
+    property(name=\"hostname\")
+    constant(value=\" \")
+    property(name=\"app-name\")
+    constant(value=\" [\")
+    property(name=\"syslogseverity-text\")
+    constant(value=\"]\")
+    constant(value=\":\")
+    property(name=\"msg\" spifno1stsp=\"on\" )
+    property(name=\"msg\" droplastlf=\"on\" )   
+    constant(value=\"\n\")
 }
 
 if \$programname == 'rusty_beagle' then /var/log/rusty_beagle.log;rusty_beagle_list
