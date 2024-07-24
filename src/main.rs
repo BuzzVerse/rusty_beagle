cbee10fe04abfefe4b5f5c1f3279a936411c2e42
DabP03 <piotrek.dabrowa@gmail.com>
2024-07-24 10:15:44 +0200

    minor refactoring

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod logging;
 
 extern crate log;
 
-pub use crate::config::{read_config, Config, MqttConfig};
+pub use crate::config::{Config, MqttConfig};
 pub use crate::defines::{api_defines::API_Status, lora_defines::LoRa_Registers};
 pub use crate::logging::start_logger;
 //use log::{debug, error, info, trace, warn};
