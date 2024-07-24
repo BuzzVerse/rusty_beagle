363fc418946ce336f5764a24bafd02e025af2039
jacek-kow <jacekkowalski.zg@gmail.com>
2024-07-24 11:07:45 +0200

    Added config file as a command line argument

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ pub use crate::config::{Config, MqttConfig};
 pub use crate::defines::{api_defines::API_Status, lora_defines::LoRa_Registers};
 pub use crate::logging::start_logger;
 //use log::{debug, error, info, trace, warn};
+use std::env;
 
 #[cfg(target_arch = "x86_64")]
 fn prepare_mocks() {
@@ -15,9 +16,22 @@ fn prepare_mocks() {
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = match args.len() {
+        2 => &args[1],
+        _ => {
+            eprintln!("Wrong number of arguments!");
+            println!("Usage: ./rusty_beagle [config file]");
+            std::process::exit(-1);
+        }
+    };
+
     start_logger();
+
     #[cfg(target_arch = "x86_64")]
     prepare_mocks();
-    let config = Config::from_file("./conf.ron");
+
+    let config = Config::from_file(file_path);
     println!("{:?}", config);
 }
